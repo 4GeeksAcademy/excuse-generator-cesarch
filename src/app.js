1d5a5804f96fcd4e62a677df10f9dec5efeec3e8
César Chavez <cesar.chavez96@icloud.com>
2025-05-26 01:03:16 +0000

    v1 excuse generator in JS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let who = ['My Lights', 'My Dog', 'My Parents', 'My Girlfriend']; 
+  let action = ['ate', 'destroyed', 'burned', 'threw away'];
+  let homework = ['my homework', 'my work', 'my hopes', 'my dinasty']; 
+  let when = ['yesterday', 'last Night', 'last week', 'this morning']; 
+
+  let randomwho = Math.floor(Math.random() * who.length); 
+  let randomaction = Math.floor(Math.random() * action.length); 
+  let randomhomework = Math.floor(Math.random() * homework.length); 
+  let randomwhen = Math.floor(Math.random() * when.length); 
+
+  document.getElementById("excuse").innerText = who[randomwho] + " " + action[randomaction] + " " + homework[randomhomework] + " " + when[randomwhen];
+}
+
